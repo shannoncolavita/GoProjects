5eff0f063f7a100d301ca96d3afebe6e42cddf64
Shannon Colavita <shannoncolavita@gmail.com>
2018-02-26 13:20:03 +0000

    Slices

--- src/helloworld.go
+++ src/helloworld.go
@@ -4,11 +4,14 @@ import "./greeting"
 
 func main() {
 	//var s = greeting.Salutation{"Bob", "Hello"}
+
 	slice := []greeting.Salutation{
 		{ "Bob", "Hello"},
 		{ "Joe", "Hi"},
 		{ "Mary", "What's up?"},
 	}
+
+	slice = append(slice[:1], slice[2:]... )
 
 	greeting.Greet(slice, greeting.CreatePrintFunction("?"), true, 6)
 
